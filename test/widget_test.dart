ee0c4d73d0c77ccac7c25d380f75b2eb67d66f28
Kiran Patil <kiran.patil@mindbowser.com>
2023-02-17 17:12:12 +0530

    file changes done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:animated_button/main.dart';
+import 'package:animated_button/animated_button.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
